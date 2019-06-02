42573486fcdb4e76495c8902d9448a80fe604d48
Tim Jester-Pfadt <tim.jester-pfadt@merckgroup.com>
2019-06-02 09:41:11 +0200

    Hex.pm meta data

--- mix.exs
+++ mix.exs
@@ -6,9 +6,23 @@ defmodule Ssz.MixProject do
       app: :ssz,
       version: "0.1.0",
       elixir: "~> 1.8",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
+      source_url: "https://github.com/timjp87/elixir-ssz",
       deps: deps()
+    ]
+  end
+
+  defp description() do
+    "WIP Implementation of the Simple Serialize specification for Ethereum 2.0."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/timjp87/elixir-ssz"}
     ]
   end
 
@@ -22,7 +36,8 @@ defmodule Ssz.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_crypto, "~> 0.10.0"}
+      {:ex_crypto, "~> 0.10.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
